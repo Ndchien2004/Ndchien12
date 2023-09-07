54eb73c0e333ad701ad8f19ca081b07c274f8ab1
Ndchien2004 <144213919+Ndchien2004@users.noreply.github.com>
2023-09-07 15:59:10 +0700

    Polymorphism

---
+++ Slot2/Polymorphism/src/polymorphism/FoodDeliveryBoy.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polymorphism;
+
+/**
+ *
+ * @author PC
+ */
+class FoodDeliveryBoy extends DeliveryBoy {
+    public void deliver() {
+        System.out.println("Delivering Food");
+    }
+}
