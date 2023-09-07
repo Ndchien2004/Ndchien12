2ff33a35c64ec75c9c9338b7bd6033d7e64231d0
Ndchien2004 <144213919+Ndchien2004@users.noreply.github.com>
2023-09-07 14:44:54 +0700

    Slot2

---
+++ Abstraction/src/abstraction/Abstraction.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstraction;
+
+/**
+ *
+ * @author PC
+ */
+public class Abstraction {
+public static void main(String args[])
+    {
+        Car obj
+            = new Honda(); // Car object =>contents of Honda
+        obj.stop(); // call the method
+    }
+}
