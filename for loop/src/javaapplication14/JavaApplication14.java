7e7e17699f3778d849de416190bb091176155fb5
Ndchien2004 <144213919+Ndchien2004@users.noreply.github.com>
2023-09-19 15:52:21 +0700

    19/9/2023 slot5

---
+++ for loop/src/javaapplication14/JavaApplication14.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication14;
+
+/**
+ *
+ * @author PC
+ */
+public class JavaApplication14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i=2;i<=10;i++) {
+            if(i%2==0)
+                System.out.println(i);
+        }
+    }
+    
+}
---
+++ while loop/src/javaapplication15/JavaApplication15.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication15;
+
+/**
+ *
+ * @author PC
+ */
+public class JavaApplication15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i=2;
+        while(i<=10){
+            if(i%2==0)
+                System.out.println(i);
+                i++;
+        }
+    }
+    
+}
